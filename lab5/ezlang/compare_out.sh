7225e809674a606d93459aeca415a3ca3f00d2fe
gomesfilipe <filipejvmf@gmail.com>
2023-06-14 23:54:51 -0300

    fixing scripts

--- lab5/ezlang/compare_out.sh
+++ lab5/ezlang/compare_out.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# ROOT=/home/filipe/git/compiladores-labs/lab5
-ROOT=/home/fgasouza/compiladores-labs/lab5
+ROOT=/home/filipe/git/compiladores-labs/lab5
+# ROOT=/home/fgasouza/compiladores-labs/lab5
 ANTLR_PATH=$ROOT/tools/antlr-4.11.1-complete.jar
 CLASS_PATH_OPTION="-cp .:$ANTLR_PATH"
 
-# GRAMMAR_NAME=EZ
 BIN_PATH=bin
 
-# DATA=/home/filipe/git/compiladores-labs/lab4/ezlang
 DATA=$ROOT/ezlang
 IN=$DATA/in
 OUT=$DATA/out05
