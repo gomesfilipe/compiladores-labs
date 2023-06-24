f3000acc39fadc7cfdd853f711c5db7b40e918a7
gomesfilipe <filipejvmf@gmail.com>
2023-06-24 17:24:10 -0300

    starting lab6

---
+++ lab6/ezlang/compare_out.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ROOT=/home/filipe/git/compiladores-labs/lab5
+# ROOT=/home/fgasouza/compiladores-labs/lab5
+ANTLR_PATH=$ROOT/tools/antlr-4.11.1-complete.jar
+CLASS_PATH_OPTION="-cp .:$ANTLR_PATH"
+
+BIN_PATH=bin
+
+DATA=$ROOT/ezlang
+IN=$DATA/in
+OUT=$DATA/out05
+
+for infile in `ls $IN/c*.ezl`; do
+    base=$(basename $infile)
+    outfile=$OUT/${base/.ezl/.out}
+    echo Running $base
+    java $CLASS_PATH_OPTION:$BIN_PATH Main < $infile 2>/dev/null | diff -w $outfile -
+done
